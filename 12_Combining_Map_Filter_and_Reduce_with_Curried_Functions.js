35197d5d913df60761b1ff086635231a6f203566
pkro <pkrombach@web.de>
2020-07-23 11:02:32 +0200

    fixed selection of weEvents (double map)

--- 12_Combining_Map_Filter_and_Reduce_with_Curried_Functions.js
+++ 12_Combining_Map_Filter_and_Reduce_with_Curried_Functions.js
@@ -1,9 +1,8 @@
-// this stuff isn't working probably because the sources are not imported correctly
-
 const { log } = console;
-const { reduce, map, filter, partial, __ } = require('./funcUtils');
-const { sources } = require('./calendar-events');
-
+import { reduce, map, filter, partial, __ } from './funcUtils.js';
+import R from 'ramda';
+import sources from './calendar-events.js';
+//log(sources);
 const curry = partial;
 
 //log(sources);
@@ -14,9 +13,9 @@ const prop = curry((key, obj) =>
   isObj(obj) && isDefined(obj[key]) ? obj[key] : undefined
 );
 
-// reverse / flipped args
+// reverse / flipped first 2 args
 //const objProp = (obj, key) => prop(key, obj);
-const flip = curry((fn) => (a, b, ...args) => fn(b, a, ...args));
+const flip = curry((fn, a, b, ...args) => fn(b, a, ...args));
 const objProp = flip(prop);
 
 // const path = (arrPath, obj) =>
@@ -31,24 +30,32 @@ const objProp = flip(prop);
 const path = curry((arrPath, obj) => reduce(objProp, obj, arrPath));
 
 const o = { a: { b: { c: 1000 } } };
-log(path(['a', 'b', 'c'], o));
+//log(path(['a', 'b', 'c'], o));
+//const t = path(['a', 'b', 'c']);
+//log(t(o));
 
 function onWeekend(day) {
   const dayNum = new Date(day).getDay();
   // 0 = sunday, 6 = saturday
   return [6, 0].some((n) => n === dayNum);
 }
+
 const startDateTime = path(['start', 'dateTime']);
 const getWeekendItems = filter((item) => onWeekend(startDateTime(item)));
 const showDateTime = (d) => new Date(d).toUTCString();
 
 const getItems = map(prop('items'));
-
+log(getItems(sources));
 //log(getItems(sources));
-const weEvents = (sources) => map(getWeekendItems, getItems(sources));
-
+const weEvents = map(getWeekendItems, getItems(sources));
+//log(weEvents);
+// remember that map is curried / partialed, so calling it without the object / array
+// to work on it returns a function that expects that object/array
 const mapDateTimeStrings = map(
-  (event) => `${prop('summay', event)} - ${showDateTime(startDateTime(event))}`
+  (event) =>
+    `${prop('summary', event)} - ${startDateTime(event)} - ${showDateTime(
+      startDateTime(event)
+    )}`
 );
 
-log(map(mapDateTimeStrings(weEvents)));
+log([].concat(...map(mapDateTimeStrings, weEvents)));
